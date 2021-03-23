d4577cf828133f724fcf587c977fd0321f71bd25
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-23 20:59:13 +0000

    fixed again

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-let trapazoidArea = function() {
-  let b1 = parseFloat(document.getElementById('b1').value);
+const trapazoidArea = function () {
+  let = parseFloat(document.getElementById('b1').value)
   let b2 = parseFloat(document.getElementById('b2').value)
   let hieght = parseFloat(document.getElementById('hieght').value)
-  let output = document.getElementById('output');
-if (isNaN(b1)|| isNaN(b2) || isNaN(hieght))
-output.textContent= "You did not enter three numbers"
-  let area=6 * b1 + b2 / 2 * hieght;
- output.textContent = "Area =" + area;
+  let output = document.getElementById('output')
+  if (isNaN(b1) | | isNaN(b2) | | isNaN(hieght))
+    output.textContent = 'You did not enter three numbers'
+  let area=6 * b1 + b2 / 2 * hieght
+  output.textContent = "Area =" + area
 }
