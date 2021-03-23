f4979a927b8755b9bdec909666e82c8be27d42b2
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-23 21:07:24 +0000

    fixed java

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const trapazoidArea = function () {
   let b2 = parseFloat(document.getElementById('b2').value)
   let hieght = parseFloat(document.getElementById('hieght').value)
   let output = document.getElementById('output')
-  if (isNaN(b1) | | isNaN(b2) | | isNaN(hieght))
+  if (isNaN(b1) || isNaN(b2) || isNaN(hieght))
     output.textContent = 'You did not enter three numbers'
   let area=6 * b1 + b2 / 2 * hieght
   output.textContent = "Area =" + area
